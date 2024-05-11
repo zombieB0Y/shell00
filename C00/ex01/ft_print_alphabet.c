45b8da0ae3400cb636f1d71e9fb69ac099c93e1e
zombieBOY <zoubairgaming5@gmail.com>
2024-05-11 22:24:49 +0100

    tt

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	first;
+
+	first = 'a';
+	while (first <= 'z')
+	{
+		ft_putchar(first);
+		first = first + 1;
+	}
+}
